3dbc1e452fdaee892e400473da9b6a71c9f3a7a7
kcieslak <kcieslak@egnyte.com>
2023-11-29 09:36:31 +0100

    formatting, remove dead code

--- src/TA-egnyte-protect/bin/input_module_egnyte.py
+++ src/TA-egnyte-protect/bin/input_module_egnyte.py
@@ -2,17 +2,15 @@
 import os
 import sys
 import time
-import datetime
 import json
 from requests.exceptions import HTTPError
-from solnlib.splunkenv import get_splunkd_uri
-from solnlib.credentials import (CredentialManager, CredentialNotExistException)
-import requests
 from ta_egnyte_protect_utility import *
 import ta_egnyte_constants as tec
 import splunk.rest as rest
 import splunklib.client as client
+
 APP_NAME = os.path.abspath(__file__).split(os.sep)[-3]
+
 
 def validate_input(helper, definition):
     interval = float(definition.parameters.get('interval', None))
@@ -20,11 +18,14 @@ def validate_input(helper, definition):
         helper.log_error("Interval must be at least 600 seconds.")
         raise Exception('Interval must be at least 600 seconds')
 
+
 def get_checkpoint(helper, stanza_name):
     return helper.get_check_point(stanza_name)
 
+
 def set_checkpoint(helper, stanza_name, state):
     return helper.save_check_point(stanza_name, state)
+
 
 def collect_events(helper, ew):
     # getting setup parameters
@@ -43,35 +44,38 @@ def collect_events(helper, ew):
     if endpoint == "US":
         base_url = tec.us_url
     else:
-        base_url =tec.europe_url
+        base_url = tec.europe_url
     auth_url = str(base_url) + "/oauth2/token"
-    
+
     checkpoint = get_checkpoint(helper, stanza_name) or dict()
 
     service = client.connect(host='localhost', port=8089, token=session_key)
 
     # Going to take access/refresh token if it is not available in the checkpoint
     if not checkpoint or str(checkpoint.get("code")) != str(code):
-        helper.log_info("Checkpoint is not available or code changed from setup page. Hence requesting new access token.")
+        helper.log_info(
+            "Checkpoint is not available or code changed from setup page. Hence requesting new access token.")
         state = get_checkpoint(helper, stanza_name) or dict()
         try:
-            response = generate_or_refresh_token(helper=helper, auth_url=auth_url, clientid=clientid, client_secret=client_secret, code=code)
-            helper.log_info("Checkpoint is not available or code changed from setup page. Hence requested new access token.")
+            response = generate_or_refresh_token(helper=helper, auth_url=auth_url, clientid=clientid,
+                                                 client_secret=client_secret, code=code)
+            helper.log_info(
+                "Checkpoint is not available or code changed from setup page. Hence requested new access token.")
             response = response.json()
             if response.get("error"):
-                helper.log_error("Error while getting access/refresh token error: {} error_description:{}".format(response.get("error", ""), response.get("error_description", "")))
+                helper.log_error("Error while getting access/refresh token error: {} error_description:{}".format(
+                    response.get("error", ""), response.get("error_description", "")))
                 helper.log_error("Please generate new code and update the input with new code.")
                 postargs = {
-                        'severity': "error",
-                        'name': APP_NAME,
-                        'value': "Egnyte Add-on: Please generate new code and update the input with new code."
+                    'severity': "error",
+                    'name': APP_NAME,
+                    'value': "Egnyte Add-on: Please generate new code and update the input with new code."
                 }
                 rest.simpleRequest('/services/messages',
-                                session_key, postargs=postargs)
+                                   session_key, postargs=postargs)
                 return
             else:
                 # state["access_token"] = response.get("access_token")
-                state["refresh_token"] = response.get("refresh_token")
                 state["code"] = code
                 set_checkpoint(helper, stanza_name, state)
 
@@ -90,7 +94,7 @@ def collect_events(helper, ew):
     final_modifiedAfter = ""
     if checkpoint.get("modifiedAfter"):
         data_url = str(base_url) + "/api/v1/issueupdates?modifiedAfter=" + str(checkpoint.get("modifiedAfter"))
-    else:  
+    else:
         data_url = str(base_url) + "/api/v1/issueupdates"
     data = {}
     modifiedAfter_done = True
@@ -109,47 +113,8 @@ def collect_events(helper, ew):
             raise e
         # retrying to get new access token if token is expired
         if data == 401:
-            refresh_token = checkpoint.get('refresh_token')
-            try:
-                response = generate_or_refresh_token(helper=helper, auth_url=auth_url, clientid=clientid, client_secret=client_secret, 
-                           refresh_token=refresh_token)
-                if response.status_code == 401 or response.status_code == 400:
-                    helper.log_error("Please generate new code and update the input with new code.")
-                    postargs = {
-                        'severity': "error",
-                        'name': APP_NAME,
-                        'value': "Egnyte Add-on: Please generate new code and update the input with new code."
-                    }
-
-                    rest.simpleRequest('/services/messages',
-                                    session_key, postargs=postargs)
-                    return 0
-                if response.status_code == 200:
-                    response=response.json()
-                    checkpoint["access_token"] = response.get("access_token")
-                    checkpoint["refresh_token"] = response.get("refresh_token")
-                    set_checkpoint(helper, stanza_name, checkpoint)
-            
-            except Exception as e:
-                raise e
-            
-            checkpoint = get_checkpoint(helper, stanza_name)
-            final_modifiedAfter = final_modifiedAfter or checkpoint.get("modifiedAfter")
-            if final_modifiedAfter:
-                data_url = str(base_url) + "/api/v1/issueupdates?modifiedAfter=" + str(final_modifiedAfter)
-            else:  
-                data_url = str(base_url) + "/api/v1/issueupdates"
-            if format_value and "modifiedAfter" in data_url and "format" not in data_url:
-                data_url = "{}&format=full".format(data_url)
-            else:
-                data_url = "{}?format=full".format(data_url)
-            try:
-                data = collect_issues(helper, checkpoint.get('access_token'), data_url)
-                if data.get("error",""):
-                   helper.log_error("Error while collecting data error: {} error_description:{}".format(response.get("error", ""), response.get("error_description", "")))
-                   return
-            except Exception as e:
-                raise e
+            helper.log_error("Please generate new code and update the input with new code.")
+            sys.exit(1)
         # indexing issues into Splunk
         if data.get("issues", ""):
             issues = data.get("issues")
@@ -159,7 +124,8 @@ def collect_events(helper, ew):
             source = "egnyte"
             sourcetype = "egnyte:protect:incidents"
             for i in issues:
-                event = helper.new_event(data=json.dumps(i), time=event_time, host=None, index=index,source=source, sourcetype=sourcetype, done=True,unbroken=True)
+                event = helper.new_event(data=json.dumps(i), time=event_time, host=None, index=index, source=source,
+                                         sourcetype=sourcetype, done=True, unbroken=True)
                 ew.write_event(event)
             number_of_events = number_of_events + event_count
             if data.get("modifiedAfter"):
